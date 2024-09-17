a933c05c677fab58e4e22efe69aaa8e3ad0e7df6
wws55555 <wws55555@wws55555ui-noteubug.local>
2024-09-17 18:03:58 +0900

    add JWT skill

---
+++ src/main/java/crud/blog/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package crud.blog.service;
+
+import crud.blog.domain.RefreshToken;
+import crud.blog.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
---
+++ src/main/java/crud/blog/service/TokenService.java
@@ -0,0 +1,26 @@
+package crud.blog.service;
+
+import crud.blog.config.jwt.TokenProvider;
+import crud.blog.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    public String createNewAccessToken(String refreshToken) {
+        if (!tokenProvider.validToken(refreshToken)) {
+            throw new IllegalArgumentException("Unexpected token");
+        }
+
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user = userService.findById(userId);
+        return tokenProvider.generateTokent(user, Duration.ofHours(2));
+    }
+}
